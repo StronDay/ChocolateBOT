52ca60173cb2e3a29587998321ba22eb48f1294c
Maksim Novokhatskii <stronday@icloud.com>
2023-08-03 21:24:36 +0300

    Prepared bot for deployment with webhook

--- source/main.py
+++ source/main.py
@@ -1,13 +1,19 @@
 import asyncio
+import os
 
 from aiogram.utils import executor
-from create_bot import dp
+from create_bot import dp, bot
 from handlers import client, admin, general, moderator
 from data_base import sql_worker
 from services import script_worker
 
-async def on_startup(_):
+async def on_startup(dp):
+    await bot.set_webhook(os.getenv("URL_APP"))
     print("[INFO] Бот вышел в онлайн")
+
+async def on_shutdown(dp):
+    print("[INFO] Бот выключился")
+    await bot.delete_webhook()
 
 sql_worker.sql_start()
 
@@ -19,5 +25,14 @@ moderator.register_handlers_client(dp)
 loop = asyncio.get_event_loop()
 loop.create_task(script_worker.schedule_task())
 
-executor.start_polling(dp, skip_updates = True, on_startup = on_startup)
+#executor.start_polling(dp, skip_updates = True, on_startup = on_startup)
+executor.start_webhook(
+    dispatcher = dp,
+    webhook_path='',
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host='0.0.0.0',
+    port=int(os.environ.get("PORT", 5000))
+)
 sql_worker.sql_close()
